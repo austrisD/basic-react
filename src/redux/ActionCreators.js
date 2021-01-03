b1e7e16d99db15b1978cb72e78455737fcd402eb
austrisd <austris.daugulis@gmail.com>
2021-01-03 19:47:17 +0200

    "Fetch Handling Errors"

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -55,8 +55,26 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {
   return fetch(baseUrl + "comments")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
     .then((response) => response.json())
-    .then((comments) => dispatch(addComments(comments)));
+    .then((comments) => dispatch(addComments(comments)))
+    .catch((error) => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
